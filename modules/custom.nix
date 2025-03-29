4a36179d4ac72f2e1ab051cadc18cc7145df0c91
Julien Jeanneret <julien.jeanneret@outlook.com>
2025-03-29 23:26:35 +0100

    feat: add more dependency

--- modules/custom.nix
+++ modules/custom.nix
@@ -4,13 +4,8 @@
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = [
-    pkgs.git
-    pkgs.vscode
-    pkgs.brave
-    pkgs.maven
-    pkgs.jetbrains.idea-ultimate
-    pkgs.chromium
-    pkgs.nodejs_22
-    pkgs.guake
+    pkgs.jetbrains.rider
+    pkgs.jetbrains.webstorm
+    pkgs.dotnetCorePackages.sdk_9_0-bin
   ];
 }
