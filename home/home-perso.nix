a90dfdca68906f1915172b2bffe01c8009e36922
Julien Jeanneret <julien.jeanneret@outlook.com>
2025-05-02 17:01:58 +0200

    fix: home version

--- home/home-perso.nix
+++ home/home-perso.nix
@@ -19,7 +19,7 @@
   # You can update home Manager without changing this value. See
   # the home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "25.056";
+  home.stateVersion = "25.05";
 
   # Let home Manager install and manage itself.
   programs.home-manager.enable = true;
